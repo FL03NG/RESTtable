e3535b86e07645c85f39020329881f120b6e4ef0
FL03NG <sih013@edu.zealand.dk>
2026-03-20 09:51:11 +0100

    testii

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTtable.Models;
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private TableRepoList _repo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repo = new TableRepoList(true); // starter med data
+        }
+        [TestMethod]
+        public void Get_MaxWeight_ShouldReturnCorrectTables()
+        {
+            var result = _repo.Get(null, 20).ToList();
+
+            Assert.AreEqual(2, result.Count);
+            Assert.IsTrue(result.All(t => t.Weight <= 20));
+        }
+    }
+}
+    
